f051cbb35f8e26835048c6be4396193be4a9991f
TBlexcelsior <lequoctuan.fwork@gmail.com>
2022-06-05 08:29:17 +0700

    Initial ROS Project

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/tblexcelsior/Project/DigitalRace/src
+# Build directory: /home/tblexcelsior/Project/DigitalRace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("lane_detect")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/tblexcelsior/Project/DigitalRace/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/tblexcelsior/Project/DigitalRace/devel/lib:$LD_LIBRARY_PATH"
+export PWD='/home/tblexcelsior/Project/DigitalRace/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/tblexcelsior/Project/DigitalRace/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/tblexcelsior/Project/DigitalRace/src:$ROS_PACKAGE_PATH"
